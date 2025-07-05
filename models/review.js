d6feec84c6f8513f76556745311522d443c0aad9
omkar-desai-7972 <desaiomkar7972@gmail.com>
2025-07-05 18:57:32 +0530

    review is updated

--- models/review.js
+++ models/review.js
@@ -1,22 +1,24 @@
-const { object } = require('joi');
-const mongoose=require('mongoose');
-const Schema=mongoose.Schema;
+const { object } = require("joi");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const reviewSchema=new Schema({
-    comment:String,
-    rating:{
-        type:Number,
-        min:1,
-        max:5,
-    },
-    createdAt:{
-        type:Date,
-        default:Date.now()
-    },
-    author:{
-        type:Schema.Types.ObjectId,
-        ref:"User"
-    }
+const reviewSchema = new Schema({
+  comment: String,
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
 
-module.exports=mongoose.model("Review",reviewSchema);
+module.exports = mongoose.model("Review", reviewSchema);
+
+//hii this is for checking
